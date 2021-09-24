7766cfc4f752dbc19afd2c77190ad9fbe5ee6870
Juan Carlos Galvis <juanc.galvis@outlook.com>
2021-09-24 08:26:19 -0500

    add ssl context utils to use with jks

---
+++ commons-jms-utils/src/main/java/co/com/bancolombia/commons/jms/security/SSLContextUtils.java
@@ -0,0 +1,51 @@
+package co.com.bancolombia.commons.jms.security;
+
+import lombok.SneakyThrows;
+
+import javax.net.ssl.CertPathTrustManagerParameters;
+import javax.net.ssl.KeyManagerFactory;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManagerFactory;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.security.KeyStore;
+import java.security.SecureRandom;
+import java.security.cert.CertPathBuilder;
+import java.security.cert.PKIXBuilderParameters;
+import java.security.cert.PKIXRevocationChecker;
+import java.security.cert.X509CertSelector;
+import java.util.EnumSet;
+
+public class SSLContextUtils {
+    public static final String TLS = "TLSv1.3";
+    public static final String JKS = "JKS";
+    public static final String PKIX = "PKIX";
+
+    @SneakyThrows
+    public static SSLContext buildSSLContextFromJks(String jksPath, String jksPassword) {
+        try (InputStream cert = new FileInputStream(jksPath)) {
+            final KeyStore caCertsKeyStore = KeyStore.getInstance(JKS);
+            if (jksPassword != null) {
+                caCertsKeyStore.load(cert, jksPassword.toCharArray());
+            }
+            final KeyManagerFactory kmf = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
+            final TrustManagerFactory tmf = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
+            CertPathBuilder cpb = CertPathBuilder.getInstance(PKIX);
+            PKIXRevocationChecker rc = (PKIXRevocationChecker) cpb.getRevocationChecker();
+            rc.setOptions(EnumSet.of(
+                    PKIXRevocationChecker.Option.PREFER_CRLS,
+                    PKIXRevocationChecker.Option.ONLY_END_ENTITY,
+                    PKIXRevocationChecker.Option.SOFT_FAIL,
+                    PKIXRevocationChecker.Option.NO_FALLBACK));
+            PKIXBuilderParameters pkixParams = new PKIXBuilderParameters(caCertsKeyStore, new X509CertSelector());
+            pkixParams.addCertPathChecker(rc);
+            if (jksPassword != null) {
+                kmf.init(caCertsKeyStore, jksPassword.toCharArray());
+            }
+            tmf.init(new CertPathTrustManagerParameters(pkixParams));
+            final SSLContext sslContext = SSLContext.getInstance(TLS);
+            sslContext.init(kmf.getKeyManagers(), tmf.getTrustManagers(), new SecureRandom());
+            return sslContext;
+        }
+    }
+}
--- examples/mq-reactive/src/main/java/co/com/bancolombia/jms/sample/app/config/Config.java
+++ examples/mq-reactive/src/main/java/co/com/bancolombia/jms/sample/app/config/Config.java
@@ -1,8 +1,14 @@
 package co.com.bancolombia.jms.sample.app.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ibm.mq.spring.boot.MQConnectionFactoryCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.jms.JMSException;
+import javax.jms.JMSRuntimeException;
+
+import static com.ibm.msg.client.wmq.common.CommonConstants.WMQ_CLIENT_RECONNECT;
 
 @Configuration
 public class Config {
@@ -10,5 +16,17 @@ public class Config {
     @Bean
     public ObjectMapper objectMapper() {
         return new ObjectMapper();
+    }
+
+    // Sample connection factory customization
+    @Bean
+    public MQConnectionFactoryCustomizer cfCustomizer() {
+        return mqConnectionFactory -> {
+            try {
+                mqConnectionFactory.setClientReconnectOptions(WMQ_CLIENT_RECONNECT);
+            } catch (JMSException e) {
+                throw new JMSRuntimeException(e.getErrorCode(), e.getMessage(), e);
+            }
+        };
     }
 }
