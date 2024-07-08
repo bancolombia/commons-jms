6a6b28c2c09b6bf971a27125c7da17f50e3aca42
maocq <johncarm@bancolombia.com.co>
2024-07-08 08:45:56 -0500

    MQRequestReplySelector getters

--- commons-jms-mq/src/main/java/co/com/bancolombia/commons/jms/mq/listeners/MQRequestReplySelector.java
+++ commons-jms-mq/src/main/java/co/com/bancolombia/commons/jms/mq/listeners/MQRequestReplySelector.java
@@ -9,11 +9,13 @@ import co.com.bancolombia.commons.jms.internal.listener.selector.strategy.Select
 import jakarta.jms.Destination;
 import jakarta.jms.Message;
 import jakarta.jms.Queue;
+import lombok.Getter;
 import lombok.extern.log4j.Log4j2;
 import reactor.core.publisher.Mono;
 
 import java.time.Duration;
 
+@Getter
 @Log4j2
 public final class MQRequestReplySelector implements MQRequestReply {
     public static final int SECONDS_TIMEOUT = 30;
